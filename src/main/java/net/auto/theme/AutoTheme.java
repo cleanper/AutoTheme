724ad6db33d51f6f647379d61405f219572efbea
YL <soyouber@outlook.com>
2025-09-30 08:03:56 +0800

    更好的构建

--- src/main/java/net/auto/theme/AutoTheme.java
+++ src/main/java/net/auto/theme/AutoTheme.java
@@ -10,6 +10,7 @@ final class AutoTheme {
     private static boolean cachedResult = false;
     private static final long CHECK_INTERVAL = 100; // 0.1秒检测间隔
 
+    @SuppressWarnings("StatementWithEmptyBody")
     static boolean dark() {
         long currentTime = System.currentTimeMillis();
         if (currentTime - lastCheckTime > CHECK_INTERVAL) {
