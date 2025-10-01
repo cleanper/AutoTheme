4b9c504522e46af83d0e6b18b88a94a7f54907c5
YLAzY <soyouber@outlook.com>
2025-10-01 20:24:54 +0800

    日常维护 | 移植1.15-1.16.5中移植的更改到1.14-1.14.4版本

--- src/main/java/net/auto/theme/mixin/ThemeMixin.java
+++ src/main/java/net/auto/theme/mixin/ThemeMixin.java
@@ -3,12 +3,16 @@ package net.auto.theme.mixin;
 import net.auto.theme.WindowOps;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class ThemeMixin {
+
+    @Unique
+    private boolean themeApplied = false;
 
     @Inject(
             method = "<init>",
@@ -17,7 +21,10 @@ public class ThemeMixin {
     private void onConstructorReturn(CallbackInfo ci) {
         MinecraftClient client = (MinecraftClient) (Object) this;
         if (client.window != null) {
-            WindowOps.apply(client.window);
+            if (!themeApplied) {
+                WindowOps.apply(client.window);
+                themeApplied = true;
+            }
         }
     }
 
